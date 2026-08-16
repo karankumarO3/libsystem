d4b95e56be15515ec3f699931a8a35bbc0d66326
Karan Kumar <102785462+karankumarO3@users.noreply.github.com>
2026-08-16 12:31:45 +0530

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2024 <a href="https://www.instagram.com/i_karan_o3/">Follow Me On Insta</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
